e8d795e5121ebaf66e6f03a731d311f64c61973e
AceandGaming <acekyranides@gmail.com>
2025-10-03 16:58:26 +0000

    Initial commit

---
+++ scripts/api/google_drive.py
@@ -0,0 +1,107 @@
+from scripts.api.drive_verify import get_drive_service
+import googleapiclient.http as driveAPI
+from aiohttp import ClientSession
+import re
+from datetime import datetime
+from scripts.paths import PENDING_DIR
+import string
+
+drive = get_drive_service()
+driveFiles = drive.files()
+
+folderIds = {
+    "evil": "16WT3-_bOG2I50YS9eBwNK9W99Uh-QhwK",
+    "neuro": "118gr4QuaGQGKfJ0X8VBCytvPjdzPayPY",
+    "duet": "16XWYR_-i0vAvKkmI9a77ZLiZTp20WHjs"
+}
+def GetAllFilesInFolder(q):
+    page_token = None
+    all_files = []
+    while True:
+        response = driveFiles.list(
+            q=q,
+            fields="nextPageToken, files(id, name)",
+            pageToken=page_token,
+            pageSize=100
+        ).execute()
+
+        all_files.extend(response.get('files', []))
+        page_token = response.get('nextPageToken', None)
+        if not page_token:
+            break
+    return all_files
+def GetAllFiles():
+    neuroFiles = GetAllFilesInFolder(f"'{folderIds["neuro"]}' in parents and mimeType='audio/mpeg'")
+    for f in neuroFiles:
+        f["folder"] = "neuro"
+
+    evilFiles = GetAllFilesInFolder(f"'{folderIds["evil"]}' in parents and mimeType='audio/mpeg'")
+    for f in evilFiles:
+        f["folder"] = "evil"
+
+    duetFiles = GetAllFilesInFolder(f"'{folderIds["duet"]}' in parents and mimeType='audio/mpeg'")
+    for f in duetFiles:
+        f["folder"] = "duet"
+
+    files = neuroFiles + evilFiles + duetFiles
+    return files
+
+def DownloadFile(file_id):
+    request = driveFiles.get_media(fileId=file_id)
+    with open(PENDING_DIR / file_id, 'wb') as f:
+        downloader = driveAPI.MediaIoBaseDownload(f, request)
+        done = False
+        while not done:
+            status, done = downloader.next_chunk()
+
+async def DownloadFileAsync(file_id, session: ClientSession):
+    url = 'https://drive.google.com/uc?id=' + file_id
+    async with session.get(url, ssl=False) as response:
+        with open(PENDING_DIR / file_id, 'wb') as f:
+            while chunk := await response.content.read(1024):
+                f.write(chunk)
+
+def SearchFiles(searchTerm: str, folder):
+    folderId = folderIds.get(folder)
+    if folderId is None:
+        raise Exception("Folder invaild. Must be 'neuro', 'evil' or 'duet")
+    files = GetAllFilesInFolder(f"'{folderId}' in parents and mimeType='audio/mpeg'")
+    matches = []
+    for file in files:
+        name = file["name"].lower().replace("(evil)", "")
+        if searchTerm.lower() in name:
+            matches.append(file)
+    return matches
+
+cutoff = datetime.strptime("21/03/24", "%d/%m/%y")
+def GenerateMetaDataFromFile(fileName):
+    songData = {}
+    fileName = fileName.replace("(evil)", "").replace(".mp3", "").strip()
+
+    regex = re.search(r"(.*?) - (.*?)\s+?\((\d\d \d\d \d\d)\)", fileName)
+    if not regex:
+        songData["title"] = fileName
+        songData["artist"] = "unknown"
+        songData["date"] = "unknown"
+        return songData
+
+    dateString = regex.group(3).strip().replace(" ", "/")
+    songData["date"] = dateString
+
+    fileDate = datetime.strptime(dateString, "%d/%m/%y")
+    mode = 2 if fileDate < cutoff else 1
+
+    if mode == 1:
+        songData["title"] = regex.group(2).strip()
+        artists = regex.group(1).split("&")
+        for i, artist in enumerate(artists):
+            artists[i] = artist.strip()
+        songData["artist"] = ", ".join(artists)
+    else:
+        songData["title"] = string.capwords(regex.group(1).strip())
+        artists = regex.group(2).split("&")
+        for i, artist in enumerate(artists):
+            artists[i] = artist.strip()
+        songData["artist"] = string.capwords(", ".join(artists))
+
+    return songData
---
+++ scripts/classes/user.py
@@ -0,0 +1,283 @@
+from __future__ import annotations
+import scripts.paths as paths
+import uuid as createUUID
+import json
+from passlib.context import CryptContext
+import secrets
+from scripts.filters import Filter
+from scripts.utils import *
+from .song import SongManager, Song
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class User:
+    @staticmethod
+    def CreateFromJson(userJson: dict):
+        if not HasValues(userJson, "uuid", "username", "password", "playlists"):
+            print(f"Warn: Invalid user json: {userJson}")
+            return
+        playlists = []
+        for playlistUUID in userJson["playlists"]:
+            playlist = PlaylistManager.GetPlaylist(playlistUUID)
+            if playlist is None:
+                print(f"Warn: Invalid playlist data in user json: {playlistUUID}")
+                return
+            playlist.userid = userJson["uuid"]
+            playlists.append(playlist)
+        return User(userJson["username"], userJson["password"], playlists, userJson["uuid"], hashed = True)
+    def __init__(self, username: str, password: str, playlists: list[Playlist]|None = None, uuid: str|None = None, hashed = False):
+        self.username = username
+        self.password = password if hashed else pwd_context.hash(password) 
+        self.playlists = playlists if playlists is not None else []
+
+        if uuid is None:
+            uuid = str(createUUID.uuid4())
+        while uuid in UserManager.users:
+            uuid = str(createUUID.uuid4())
+        self.uuid = uuid
+    def __repr__(self):
+        return self.username
+    def __eq__(self, other):
+        if not isinstance(other, User) or not isinstance(self, User):
+            return False
+        return self.uuid == other.uuid
+    def AddPlaylist(self, playlist: Playlist):
+        self.playlists.append(playlist)
+    def RemovePlaylist(self, playlist: Playlist):
+        self.playlists.remove(playlist)
+    def VerifyPassword(self, password: str):
+        return pwd_context.verify(password, self.password)
+    def ToJson(self):
+        playlistUUIDs = [playlist.uuid for playlist in self.playlists]
+        return {
+            "uuid": self.uuid,
+            "username": self.username,
+            "password": self.password,
+            "playlists": playlistUUIDs
+        }
+    def ToNetworkDict(self, lite = False):
+        playlistJSONs = [
+            playlist.ToNetworkDict() if not lite else playlist.uuid
+            for playlist in self.playlists
+        ]
+        return {
+            "uuid": self.uuid,
+            "username": self.username,
+            "playlists": playlistJSONs
+        }
+
+class UserManager:
+    users = {}
+    @staticmethod
+    def GetUser(uuid: str):
+        return UserManager.users.get(uuid)
+    @staticmethod
+    def GetUserWithUsername(username: str):
+        for user in UserManager.users.values():
+            if user.username == username:
+                return user
+    @staticmethod
+    def AddUser(user: User):
+        UserManager.users[user.uuid] = user
+        UserManager.Save()
+    @staticmethod
+    def RemoveUser(user: User):
+        for playlist in user.playlists:
+            PlaylistManager.RemovePlaylist(playlist)
+        del UserManager.users[user.uuid]
+        UserManager.Save()
+    @staticmethod
+    def GetUsers(uuidOnly = False, filters: list[Filter] = []):
+        matches = []
+        for user in UserManager.users.values():
+            for filter in filters:
+                if not filter.Match(getattr(user, filter.field, None)):
+                    break
+            else:
+                if uuidOnly:
+                    matches.append(user.uuid)
+                else:
+                    matches.append(user)
+        return matches
+    @staticmethod
+    def ConvertToJson(users: list[User]|None = None):
+        if users is None:
+            users = list(UserManager.users.values())
+        return [user.ToJson() for user in users]
+    @staticmethod
+    def ConvertToNetworkDict(users: list[User]|None = None, lite = False):
+        if users is None:
+            users = list(UserManager.users.values())
+        return [user.ToNetworkDict(lite) for user in users]
+    @staticmethod
+    def Load():
+        with open(paths.USERS_FILE, "r") as file:
+            users = json.load(file)
+            for userJson in users:
+                user = User.CreateFromJson(userJson)
+                if user is None:
+                    raise Exception(f"Loaded invalid user json: {userJson}")
+                UserManager.users[user.uuid] = user
+    @staticmethod
+    def Save():
+        with open(paths.USERS_FILE, "w") as file:
+            json.dump(UserManager.ConvertToJson(), file, indent=2)
+    
+class SessionToken:
+    def __init__(self, user: User):
+        self.token = secrets.token_urlsafe(16)
+        self.user = user
+    def __repr__(self):
+        return self.token
+    def __eq__(self, other):
+        if not isinstance(other, SessionToken) or not isinstance(self, SessionToken):
+            return False
+        return self.token == other.token
+
+class SessionManager:
+    tokens = []
+    @staticmethod
+    def AddToken(token: SessionToken):
+        SessionManager.tokens.append(token)
+    @staticmethod
+    def RemoveToken(token: SessionToken):
+        SessionManager.tokens.remove(token)
+    @staticmethod
+    def Login(username: str, password: str):
+        user = UserManager.GetUserWithUsername(username)
+        if user is None:
+            return None
+        if not user.VerifyPassword(password):
+            return None
+        for token in SessionManager.tokens:
+            if token.user == user:
+                return token.token
+        token = SessionToken(user)
+        SessionManager.AddToken(token)
+        return token.token
+    @staticmethod
+    def Logout(tokenString: str):
+        for token in SessionManager.tokens:
+            if token.token == tokenString:
+                SessionManager.RemoveToken(token)
+                return
+    @staticmethod
+    def GetUser(tokenString: str):
+        for token in SessionManager.tokens:
+            if token.token == tokenString:
+                return token.user
+    @staticmethod
+    def TokenIsValid(tokenString: str):
+        return SessionManager.GetUser(tokenString) is not None
+        
+
+
+class Playlist:
+    @property
+    def user(self) -> User:
+        user = UserManager.GetUser(self.userid)
+        if user is None:
+            raise Exception(f"Playlist has invalid user uuid: {self.userid}")
+        return user
+    @staticmethod
+    def CreateFromJson(json):
+        if not HasValues(json, "uuid", "title", "songs"):
+            print(f"Warn: Invalid playlist json: {json}")
+            return
+        songs = []
+        for songUUID in json["songs"]:
+            song = SongManager.GetSong(songUUID)
+            if song is None:
+                print(f"Warn: Invalid song data in playlist json: {json}")
+                return
+            songs.append(song)
+        return Playlist(json["title"], "", songs, json["uuid"])
+    def __init__(self, title: str, userid: str, songs: list[Song]|None = None, uuid: str|None = None):
+        self.title = title
+        self.songs = songs if songs is not None else []
+        self.userid = userid
+
+        if uuid is None:
+            uuid = str(createUUID.uuid4())
+        while uuid in PlaylistManager.playlists:
+            uuid = str(createUUID.uuid4())
+        self.uuid = uuid
+    def __repr__(self):
+        return f"{self.title} with {len(self.songs)} songs"
+    def __eq__(self, other):
+        if not isinstance(other, Playlist) or not isinstance(self, Playlist):
+            return False
+        return self.uuid == other.uuid
+    def AddSong(self, song: Song):
+        self.songs.append(song)
+    def RemoveSong(self, song: Song):
+        self.songs.remove(song)
+    def ToJson(self):
+        uuids = [song.uuid for song in self.songs]
+        return {
+            "uuid": self.uuid,
+            "title": self.title,
+            "songs": uuids
+        }
+    def ToNetworkDict(self, lite = False):
+        songJSONs = [
+            song.ToNetworkDict() if not lite else song.uuid
+            for song in self.songs
+        ]
+        return {
+            "uuid": self.uuid,
+            "title": self.title,
+            "user": self.userid,
+            "songs": songJSONs
+        }
+    
+class PlaylistManager:
+    playlists: dict[str, Playlist] = {}
+    @staticmethod
+    def GetPlaylist(uuid: str):
+        return PlaylistManager.playlists.get(uuid)
+    @staticmethod
+    def AddPlaylist(playlist: Playlist):
+        PlaylistManager.playlists[playlist.uuid] = playlist
+        playlist.user.AddPlaylist(playlist)
+        PlaylistManager.Save()
+    @staticmethod
+    def RemovePlaylist(playlist: Playlist):
+        del PlaylistManager.playlists[playlist.uuid]
+        playlist.user.RemovePlaylist(playlist)
+        PlaylistManager.Save()
+    @staticmethod
+    def GetPlaylists(filters: list[Filter] = [], playlists: list[Playlist]|None = None):
+        matches: list[Playlist] = []
+        if playlists is None:
+            playlists = list(PlaylistManager.playlists.values())
+        for playlist in playlists:
+            for filter in filters:
+                if not filter.Match(getattr(playlist, filter.field, None)):
+                    break
+            else:
+                matches.append(playlist)
+        return matches
+    @staticmethod
+    def ConvertToJson(playlists: list[Playlist]|None = None):
+        if playlists is None:
+            playlists = list(PlaylistManager.playlists.values())
+        return [playlist.ToJson() for playlist in playlists]
+    @staticmethod
+    def ConvertToNetworkDict(playlists: list[Playlist]|None = None, lite = False):
+        if playlists is None:
+            playlists = list(PlaylistManager.playlists.values())
+        return [playlist.ToNetworkDict(lite) for playlist in playlists]
+    @staticmethod
+    def Save():
+        with open(paths.PLAYLISTS_FILE, "w") as file:
+            json.dump(PlaylistManager.ConvertToJson(), file, indent=2)
+    @staticmethod
+    def Load():
+        with open(paths.PLAYLISTS_FILE, "r") as file:
+            playlists = json.load(file)
+            for playlistJson in playlists:
+                playlist = Playlist.CreateFromJson(playlistJson)
+                if playlist is None:
+                    raise Exception(f"Loaded invalid playlist json: {playlistJson}")
+                PlaylistManager.playlists[playlist.uuid] = playlist
