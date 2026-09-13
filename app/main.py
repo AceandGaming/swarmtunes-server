259ee15e3b51dcf9e7df9b4314925ec15c07ddc0
AceandGaming <acekyranides@gmail.com>
2026-09-13 21:42:37 +1000

    tauri

--- app/main.py
+++ app/main.py
@@ -52,7 +52,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://swarmtunes.com"],
+    allow_origins=["https://swarmtunes.com", "tauri://localhost"],
     allow_origin_regex=r"^https://[a-zA-Z0-9-]+\.swarmtunes-client\.pages\.dev$",  # Cloudflare Pages
     allow_credentials=True,
     allow_methods=["*"],
